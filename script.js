e84b0ac93a06efcf59ebcfd370d8428e41453ebb
Connor Carciofini <ccarciofini@gmail.com>
2022-06-27 19:12:37 -0500

    almost there

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 var lettersUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var Symbols = ['!', '@', '#', '$', '%', '^', '&', '*','_', '-', '+', '=', '?']
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*','_', '-', '+', '=', '?']
 var generateBtn = document.querySelector("#generate"); 
 
 characterAmountNumber.addEventListener('input', syncRange);
@@ -18,19 +18,48 @@ function syncRange(event) {
     characterAmountNumber.value = value;
     characterAmountRange.value = value;
 }
+
+
+
 function generatePassword() {
-    passwordResult = ''
-    for (var index = 0; index < characterAmountNumber.value; index++) {
-        passwordResult = letters[Math.floor(Math.random()*26)]
-
-
-
-
-        console.log(passwordResult)
-    }   
-    
-    
+    var passwordRaw = letters
+    if (includeUppercase.checked) passwordRaw = letters.concat(lettersUppercase);
+    if (includeNumbers.checked) passwordRaw = letters.concat(numbers);
+    if (includeSymbols.checked) passwordRaw = letters.concat(symbols);
+    if (includeUppercase.checked && includeNumbers.checked) passwordRaw = letters.concat(lettersUppercase, numbers)
+    if (includeUppercase.checked && includeSymbols.checked) passwordRaw = letters.concat(lettersUppercase, symbols);
+    if (includeNumbers.checked && includeSymbols.checked) passwordRaw = letters.concat(numbers, symbols);
+    if (includeUppercase.checked && includeNumbers.checked && includeSymbols.checked)
+        passwordRaw = letters.concat(lettersUppercase, numbers, symbols);
+    for (index = 0; index < characterAmountNumber.value; index++) {
+        passwordResult = passwordRaw [(Math.floor(Math.random() * passwordRaw.length))]
+        
     }
+    console.log(passwordRaw)
+    return passwordResult;
+   
+    }
+    
+
+
+    
+   
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
     
 
     // passwordResult = letters[Math.floor(Math.random())* characterAmountNumber.value] 
